a323e9fed6391671db7ad6d071ad6e53e24c26dc
Magaiver <magaivermagalhaes.mm@gmail.com>
2025-03-10 21:47:21 -0300

    feat: ensure same product can be added more than once to cart

--- src/app/[slug]/menu/contexts/cart.tsx
+++ src/app/[slug]/menu/contexts/cart.tsx
@@ -1,48 +1,66 @@
-"use client"
+"use client";
 
 import { Product } from "@prisma/client";
-import { createContext,ReactNode, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
-interface CartProduct extends Pick<Product, "id" | "name" | "price" | "imageUrl">{
-    quantity: number;
+interface CartProduct
+  extends Pick<Product, "id" | "name" | "price" | "imageUrl"> {
+  quantity: number;
 }
 
-
-export interface IcartContext {
-    isOpen: boolean;
-    products: CartProduct[];
-    toggleCart: () => void;
-    addProduct: (product: CartProduct) => void;
+export interface ICartContext {
+  isOpen: boolean;
+  products: CartProduct[];
+  toggleCart: () => void;
+  addProduct: (product: CartProduct) => void;
 }
 
-export const CartContext = createContext<IcartContext>({
-    isOpen: false,
-    products: [],
-    toggleCart: () => {},
-    addProduct: () => {},
-   
-})
+export const CartContext = createContext<ICartContext>({
+  isOpen: false,
+  products: [],
+  toggleCart: () => {},
+  addProduct: () => {},
+});
 
-export const CartProvider = ({children}: {children: ReactNode}) => {
-    const [products, setProducts] = useState<CartProduct[]>([]);
-    const [isOpen, setIsOpen] = useState<boolean>(false);
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
 
-    const toggleCart = () => {
-        setIsOpen(prev => !prev)
+  const toggleCart = () => {
+    setIsOpen((prev) => !prev);
+  };
+  const addProduct = (product: CartProduct) => {
+    // verificar se o produto ja esta no carrinho
+    // se estiver, aumentar sua quantidade
+    // se nao estiver adicione
+    const productIsAlreadyOnTheCart = products.some(
+      (prevProduct) => prevProduct.id === product.id,
+    );
+    if (!productIsAlreadyOnTheCart) {
+      return setProducts((prev) => [...prev, product]);
     }
-
-    const addProduct = (product: CartProduct) => {
-        setProducts((prev) => [...prev, product])
-    }
-    return (
-        <CartContext.Provider
-            value={{
-                isOpen,
-                products,
-                toggleCart,
-                addProduct,
-            }}>
-            {children}
-        </CartContext.Provider>
-    )
-}
+    setProducts((prevProducts) => {
+      return prevProducts.map((prevProduct) => {
+        if (prevProduct.id === product.id) {
+          return {
+            ...prevProduct,
+            quantity: prevProduct.quantity + product.quantity,
+          };
+        }
+        return prevProduct;
+      });
+    });
+  };
+  return (
+    <CartContext.Provider
+      value={{
+        isOpen,
+        products,
+        toggleCart,
+        addProduct,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
