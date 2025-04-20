903e6df11a6b0b2ff1216595a5c2b8cdd23ccbc2
Magaiver <magaivermagalhaes.mm@gmail.com>
2025-04-20 16:43:58 -0300

    feat: redirect homepage to /maga-donalds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-const HomePage = () => {
-  return <h1>Hello World!!!</h1>;
+import { redirect } from "next/navigation";
+
+export default function HomePage() {
+  redirect("/maga-donalds");
 }
- 
-export default HomePage;
