5532c938ba0a7bef7acf475dc96eb09add36e42e
Magaiver <magaivermagalhaes.mm@gmail.com>
2025-04-20 15:29:23 -0300

    chore: add postinstall with prisma generate script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased`}
+        suppressHydrationWarning={true} className={`${poppins.className} antialiased`}
       >
         <CartProvider>
           {children}
